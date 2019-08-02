88c82dda559cb60b555f143e23d5299d1beb1c3f
tanvir-ux <tanviralamshuvo@gmail.com>
2019-08-03 02:48:54 +0600

    Code Refactored

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,75 +2,47 @@ import { Component } from '@angular/core';
 import { NavController,LoadingController } from 'ionic-angular';
 import { RedditProvider } from '../../providers/reddit/reddit';
 
-
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
 	news: any = [];
 	newNews: any = [];
 	next: string;
-	
 
   constructor(public navCtrl: NavController,
-   public reddit: RedditProvider,public loadingCtrl: LoadingController) {
+   public reddit: RedditProvider,
+   public loadingCtrl: LoadingController){ }
 
-  }
-
-  ionViewDidLoad(){
-
-  	
+  ionViewDidLoad(){  	
   	this.loadMovies();
-
-
   }
-
-
 
   loadMovies(){
   	this.reddit.getMovies()
   				.subscribe((data) => {
-  					this.news = data.results;
-  					// console.log(data);
-  					// console.log(this.news);
-  					// console.log(data.next);
+  					this.news = data.results;  					
   					this.next = data.next;
-
   				})
   }
 
   loadNext(){
   	this.reddit.getNext(this.next)
-  				.subscribe((results) => {
-  					console.log(results.results);
+  				.subscribe((results) => {  					
   					this.newNews = results.results;
   					this.news = this.newNews.concat(this.news);
-  					this.next = results.next;
-  					
+  					this.next = results.next;  					
   				})
-
-  	this.presentLoadingBubbles();			
-
+  	this.presentLoadingBubbles();
   }
-
 
   presentLoadingBubbles() {
     let loading = this.loadingCtrl.create({
       spinner: 'bubbles',
       content: 'Loading ...',
-      duration: 1000
-     
-    });
-
+      duration: 1000     
+	});	
     loading.present();
-
-
-
-
   }
-
-
-
 }
--- src/providers/reddit/reddit.ts
+++ src/providers/reddit/reddit.ts
@@ -2,31 +2,18 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the RedditProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular DI.
-*/
 @Injectable()
 export class RedditProvider {
-
-	url: string = 'https://swapi.co/api/people';
-	
-
-
+  url: string = 'https://swapi.co/api/people';
 
   constructor(public http: Http) {
     console.log('Hello RedditProvider Provider');
   }
-
-
+  
   getMovies(){
   	return this.http.get(this.url)
   					.map(res => res.json());
   }
-
-
   getNext(next){
   	
   	return this.http.get(next)
